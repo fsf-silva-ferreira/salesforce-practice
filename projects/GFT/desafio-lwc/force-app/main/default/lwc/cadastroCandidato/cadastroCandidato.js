5f0885f0749372ca3e44657c994f61815c84573a
fsf.silva.ferreira@gmail.com <fsf.silva.ferreira@gmail.com>
2021-12-10 18:02:39 -0300

    GFT Challenge: Candidate form created; Imported Candidato object schema in JS.

--- projects/GFT/desafio-lwc/force-app/main/default/lwc/cadastroCandidato/cadastroCandidato.js
+++ projects/GFT/desafio-lwc/force-app/main/default/lwc/cadastroCandidato/cadastroCandidato.js
@@ -1,3 +1,23 @@
+/**
+    * @description      : 
+    * @author           : fodf
+    * @group            : 
+    * @created          : 10/12/2021 - 17:39:50
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 10/12/2021
+    * - Author          : fodf
+    * - Modification    : 
+**/
 import { LightningElement } from 'lwc';
+import { createRecord } from 'lightning/uiRecordApi';
+
+import CANDIDATO_OBJECT from '@salesforce/schema/Candidato__c';
+import NAME_FIELD from '@salesforce/schema/Candidato__c.name';
+import SEXO_FIELD from '@salesforce/schema/Candidato__c.sexo__c';
+import CARGO_FIELD from '@salesforce/schema/Candidato__c.cargo__c';
+
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class CadastroCandidato extends LightningElement {}
