cdc1c356d05e53e5a85349c268f01ff9eca1621d
Flávio Ferreira <fsf.silva.ferreira@gmail.com>
2023-03-20 18:42:27 -0300

    lwc-laboratory: Components 'childHelloWorld' and 'parentHelloWorld'.

---
+++ projects/lwc-aboratory/force-app/main/default/lwc/childHelloWorld/childHelloWorld.js
@@ -0,0 +1,6 @@
+// helloWorld.js
+import { LightningElement, api } from "lwc";
+
+export default class HelloWorld extends LightningElement {
+    @api firstName = "World";
+}
---
+++ projects/lwc-aboratory/force-app/main/default/lwc/parentHelloWorld/parentHelloWorld.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class ParentFirstComponent extends LightningElement {}
