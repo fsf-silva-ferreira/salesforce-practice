5b27ff9f918b0b632aeb3879889ea29f52f773c8
Flávio Ferreira <fsf.silva.ferreira@gmail.com>
2023-03-23 08:28:01 -0300

    Component 'myLightningAccordion'.

---
+++ projects/lwc-laboratory/force-app/main/default/lwc/myLightningAccordion/myLightningAccordion.js
@@ -0,0 +1,15 @@
+import { LightningElement } from 'lwc';
+
+export default class LightningExampleAccordionBasic extends LightningElement {
+    activeSectionMessage = '';
+
+    handleToggleSection(event) {
+        this.activeSectionMessage = 'Open section name:  ' + event.detail.openSections;
+    }
+
+    handleSetActiveSectionC() {
+        const accordion = this.template.querySelector('.example-accordion');
+
+        accordion.activeSectionName = 'C';
+    }
+}
