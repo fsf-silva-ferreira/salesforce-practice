4e9bd5bafbeae82d7ba4b2b637c470c1ed2fd654
Flávio Ferreira <fsf.silva.ferreira@gmail.com>
2023-03-24 08:08:11 -0300

    Component 'alert'.

---
+++ projects/lwc-laboratory/force-app/main/default/lwc/alert/alert.js
@@ -0,0 +1,14 @@
+import { LightningElement } from 'lwc';
+import LightningAlert from 'lightning/alert';
+
+export default class MyAlert extends LightningElement {
+    
+    async handleAlertClick() {
+        await LightningAlert.open({
+            message: 'this is the alert message',
+            theme: 'error', // a red theme intended for error states
+            label: 'Error!', // this is the header text
+        });
+        //Alert has been closed
+    }
+}
