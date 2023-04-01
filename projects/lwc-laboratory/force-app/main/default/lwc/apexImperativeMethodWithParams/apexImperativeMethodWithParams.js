761a903efee57687255d71b3e5bfe093b32ff355
Flávio Ferreira <fsf.silva.ferreira@gmail.com>
2023-04-01 12:38:49 -0300

    Add doc and code links in comments to ApexImperativeMethodWithParams JS class.

--- projects/lwc-laboratory/force-app/main/default/lwc/apexImperativeMethodWithParams/apexImperativeMethodWithParams.js
+++ projects/lwc-laboratory/force-app/main/default/lwc/apexImperativeMethodWithParams/apexImperativeMethodWithParams.js
@@ -1,7 +1,12 @@
+/*
+ * https://developer.salesforce.com/docs/component-library/documentation/en/lwc/lwc.apex_call_imperative
+ * https://github.com/trailheadapps/lwc-recipes/tree/main/force-app/main/default/lwc/apexImperativeMethodWithParams
+ */
 import { LightningElement } from 'lwc';
 import findContacts from '@salesforce/apex/ContactController.findContacts';
 
 export default class ApexImperativeMethodWithParams extends LightningElement {
+    
     searchKey = '';
     contacts;
     error;
