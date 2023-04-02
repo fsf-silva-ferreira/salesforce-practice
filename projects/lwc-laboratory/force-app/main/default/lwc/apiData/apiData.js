21d8b03bd62ccac73cd169e66932a1a529b2fd12
Flávio Ferreira <fsf.silva.ferreira@gmail.com>
2023-04-02 13:04:56 -0300

    Component 'apiData'.

---
+++ projects/lwc-laboratory/force-app/main/default/lwc/apiData/apiData.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+import callApiSuccessResponseTrue from '@salesforce/apex/ApiCallExample.callApiSuccessResponseTrue';
+import callApiSuccessResponseFalse from '@salesforce/apex/ApiCallExample.callApiSuccessResponseFalse';
+import callApiErrorResponse from '@salesforce/apex/ApiCallExample.callApiErrorResponse';
+
+export default class ApiData extends LightningElement {
+
+}
