9e131b9f2c9e08d6b969a2cec5ba988f1542a31f
Flávio Ferreira <fsf.silva.ferreira@gmail.com>
2023-04-04 21:45:32 -0300

    Component 'apiData' done.

--- projects/lwc-laboratory/force-app/main/default/lwc/apiData/apiData.js
+++ projects/lwc-laboratory/force-app/main/default/lwc/apiData/apiData.js
@@ -1,43 +1,62 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 import callApiSuccessResponseTrue from '@salesforce/apex/ApiCallExample.callApiSuccessResponseTrue';
 import callApiSuccessResponseFalse from '@salesforce/apex/ApiCallExample.callApiSuccessResponseFalse';
 import callApiErrorResponse from '@salesforce/apex/ApiCallExample.callApiErrorResponse';
 
 export default class ApiData extends LightningElement {
 
-    user;
+    userName;
+    userEmail;
+    userPhone;
+
+    responseBody;
+
+    http500ErrorMessage;
+    apiErrorMessage;
     error;
 
     connectedCallback() {
+        this.callSuccessTrue();
+        this.callSuccessFalse();
+        this.callSuccessError();
     }
     
     callSuccessTrue() {
+        let user;
+
         callApiSuccessResponseTrue()
 			.then(result => {
+                this.responseBody = JSON.parse(result.responseBody);
+                user = this.responseBody.data.user;
 
+                this.userName = user.name;
+                this.userEmail = user.email;
+                this.userPhone = user.mobile;
 			})
 			.catch(error => {
-
+                this.error = error;
 			})
     }
 
     callSuccessFalse() {
         callApiSuccessResponseFalse()
 			.then(result => {
-
+                this.responseBody = JSON.parse(result.responseBody);
+                this.apiErrorMessage = this.responseBody.message;
 			})
 			.catch(error => {
-
+                this.error = error;
 			})
     }
 
     callSuccessError() {
         callApiErrorResponse()
 			.then(result => {
-
+                this.responseBody = JSON.parse(result.responseBody);
+                this.http500ErrorMessage = this.responseBody.error;
 			})
 			.catch(error => {
-
+                this.error = error;
 			})
     }
 }
