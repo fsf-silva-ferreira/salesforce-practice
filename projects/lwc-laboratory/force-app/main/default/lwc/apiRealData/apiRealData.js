914175e45aa83198d4f6e08098a52fa5a89331c4
Flávio Ferreira <fsf.silva.ferreira@gmail.com>
2023-05-06 10:32:27 -0300

    Written JS code to get properties 'cep' and 'logradouro' from the API response; Variable 'address' referenced on HTML page.

--- projects/lwc-laboratory/force-app/main/default/lwc/apiRealData/apiRealData.js
+++ projects/lwc-laboratory/force-app/main/default/lwc/apiRealData/apiRealData.js
@@ -3,9 +3,22 @@ import getZipCode from '@salesforce/apex/ApiCallExample.lookupZipCode';
 
 export default class ApiRealData extends LightningElement {
 
-    apiResponse;
+    apiResponseBody;
+    error;
+    address;
 
     connectedCallback() {
-    
+        getZipCode();
+    }
+
+    getZipCode() {
+        lookupZipCode()
+            .then(result => {
+                this.apiResponseBody = JSON.parse(result.responseBody);
+                this.address = this.apiResponseBody.cep + ' ' + this.apiResponseBody.logradouro;
+            })
+            .catch(error => {
+                this.error = error;
+            })
     }
 }
