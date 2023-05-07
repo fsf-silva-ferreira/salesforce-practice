cf8f7a82398a8bcb3b4486fded699a494f94475b
Flávio Ferreira <fsf.silva.ferreira@gmail.com>
2023-05-07 08:11:14 -0300

    Removed track and wire decorators.

--- projects/lwc-laboratory/force-app/main/default/lwc/apiMockData/apiMockData.js
+++ projects/lwc-laboratory/force-app/main/default/lwc/apiMockData/apiMockData.js
@@ -1,4 +1,4 @@
-import { LightningElement, api, track } from 'lwc';
+import { LightningElement} from 'lwc';
 import callApiSuccessResponseTrue from '@salesforce/apex/ApiCallExample.callApiSuccessResponseTrue';
 import callApiSuccessResponseFalse from '@salesforce/apex/ApiCallExample.callApiSuccessResponseFalse';
 import callApiErrorResponse from '@salesforce/apex/ApiCallExample.callApiErrorResponse';
