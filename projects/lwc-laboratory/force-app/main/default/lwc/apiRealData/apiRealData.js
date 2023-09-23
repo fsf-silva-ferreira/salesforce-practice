c30659a696abbea655de6327d36b6904c37331c0
Flávio Ferreira <fsf.silva.ferreira@gmail.com>
2023-09-23 13:22:54 -0300

    Component displaying address from API; apiRealData.js: new async  method 'handleButtonClick'.

--- projects/lwc-laboratory/force-app/main/default/lwc/apiRealData/apiRealData.js
+++ projects/lwc-laboratory/force-app/main/default/lwc/apiRealData/apiRealData.js
@@ -1,17 +1,40 @@
 import { LightningElement } from 'lwc';
+import LightningAlert from 'lightning/alert';
 import lookupAddressByZip from '@salesforce/apex/ApiCallExample.lookupAddressByZip';
 
 export default class ApiRealData extends LightningElement {
 
     zipCode;
+    //zipCodeInput
     apiResponseBody;
     error;
     address;
 
-    connectedCallback() {
-        this.getAddress();
+    //Event handlers
+    handleKeyChange(event) {
+        this.zipCode = event.target.value;
     }
 
+    async handleButtonClick() {
+         this.address = '';
+
+        if(this.zipCode) {
+            this.getAddress();    
+        } else {
+            await LightningAlert.open({
+                message: 'Zip code required.',
+                theme: 'warning',
+                label: 'Field Required',
+            });
+        }
+    }
+
+    handleOnloadEvent() {
+        zipCodeInput = this.template.querySelector('lightning-input');
+        zipCodeInput.focus();
+    }
+
+    //Custom logic
     getAddress() {
         lookupAddressByZip
         (
@@ -24,8 +47,9 @@ export default class ApiRealData extends LightningElement {
 
                 console.log('apiResponseBody:', this.apiResponseBody);
 
-                this.address = this.apiResponseBody.cep + ' - ' + this.apiResponseBody.logradouro + ' - ' +
-                    this.apiResponseBody.localidade + ' (' + this.apiResponseBody.uf + ')';
+                var logradouro = this.apiResponseBody.logradouro ? ' - ' + this.apiResponseBody.logradouro : '';
+                this.address = this.apiResponseBody.cep + logradouro + ' - ' + this.apiResponseBody.localidade + 
+                    ' (' + this.apiResponseBody.uf + ')';
             })
             .catch(error => {
                 this.error = error;
