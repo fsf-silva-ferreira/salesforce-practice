6dd5d6a9c098c9fbc36566d83f11721b152d6bc7
Flávio Ferreira <fsf.silva.ferreira@gmail.com>
2023-10-01 11:56:48 -0300

    LWC component 'apexImperativeMethodWithParams'.

--- projects/lwc-laboratory/force-app/main/default/lwc/apexImperativeMethodWithParams/apexImperativeMethodWithParams.js
+++ projects/lwc-laboratory/force-app/main/default/lwc/apexImperativeMethodWithParams/apexImperativeMethodWithParams.js
@@ -1,34 +1,45 @@
-/*
- * https://developer.salesforce.com/docs/component-library/documentation/en/lwc/lwc.apex_call_imperative
- * https://github.com/trailheadapps/lwc-recipes/tree/main/force-app/main/default/lwc/apexImperativeMethodWithParams
- */
 import { LightningElement } from 'lwc';
 import findContacts from '@salesforce/apex/ContactController.findContacts';
 
 export default class ApexImperativeMethodWithParams extends LightningElement {
-    
+
+    //Page variables
     searchKey = '';
     contacts;
     error;
 
-    handleKeyChange(event) {
-        this.searchKey = event.target.value;
+
+    //Getters
+    get isContactsFound() {
+        return this.contacts && this.contacts.length > 0;
     }
 
-    handleSearch() {
-        findContacts({ searchKey: this.searchKey })
-            .then((result) => {
-                if(result.length > 0) {
-                    this.contacts = result;
-                } else {
-                    this.contacts = undefined;
-                }
+    get isContactsEmpty() {
+        return this.contacts && this.contacts.length == 0;
+    }
 
-                this.error = undefined;
-            })
-            .catch((error) => {
-                this.error = error;
-                this.contacts = undefined;
-            });
+
+    //Event handlers
+    handleKeyChange(event) {
+        this.searchKey = event.target.value;
+        this.setUndefinedToContactsIfSearchKeyEmpty();
+    }
+
+    
+    //Custom logic
+    async handleSearch() {
+        try {
+            this.contacts = await findContacts({ searchKey: this.searchKey });
+            this.error = undefined;
+        } catch (error) {
+            this.error = error;
+            this.contacts = undefined;
+        }
+    }
+
+    setUndefinedToContactsIfSearchKeyEmpty() {
+        if(this.searchKey === '') {
+            this.contacts = undefined;
+        }
     }
 }
