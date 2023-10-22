5841eb36380a3c19f72416d730dc22ac21e7e93e
Flávio da Silva Ferreira <fsf.silva.ferreira@gmail.com>
2023-10-22 11:14:39 -0300

    Added styles and data to 'build-lwc' app.

--- development/projects/lwc/build-lwc/force-app/main/default/lwc/selector/selector.js
+++ development/projects/lwc/build-lwc/force-app/main/default/lwc/selector/selector.js
@@ -1,11 +1,22 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import Id from '@salesforce/user/Id';
+import USER_NAME from '@salesforce/schema/User.Name';
 
 
+const fields = [USER_NAME];
 export default class Selector extends LightningElement {
-    
+
+    @wire(getRecord, { recordId: '$userId', fields })
+    user;
     selectedProductId;
+    userId = Id;
 
     handleProductSelected(evt) {
         this.selectedProductId = evt.detail;
+    }
+  
+    get name() {
+        return getFieldValue(this.user.data, USER_NAME);
     }
 }
