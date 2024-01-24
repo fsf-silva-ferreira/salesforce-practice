1b2785ac783ad7b0d7285b0e43e974057c4bfce8
Flávio da Silva Ferreira <fsf.silva.ferreira@gmail.com>
2024-01-24 07:09:17 -0300

    Udemy LWC Course: Lesson 28 - Changes to palettePublisher component.

--- development/projects/lwc/lwc-course-udemy/lwc-course-part2/force-app/main/default/lwc/palettePublisher/palettePublisher.js
+++ development/projects/lwc/lwc-course-udemy/lwc-course-part2/force-app/main/default/lwc/palettePublisher/palettePublisher.js
@@ -3,33 +3,37 @@ import { fireEvent } from 'c/pubsub';
 import {CurrentPageReference} from 'lightning/navigation';
 
 
+const colorCodeOptions = 
+[
+    {
+        label: "Green",
+        value: "green"
+    },
+    {
+        label: "Red",
+        value: "red"
+    },
+    {
+        label: "Yellow",
+        value: "yellow"
+    },
+    {
+        label: "Blue",
+        value: "blue"
+    }
+]
+
 export default class PalettePublisher extends LightningElement {
 
+    //Palette properties
     colorSelectedToCanvas;
+    colorOptions = colorCodeOptions;
 
-    colorCodeOptions = 
-    [
-        {
-            label: "Green",
-            value: "green"
-        },
-        {
-            label: "Red",
-            value: "red"
-        },
-        {
-            label: "Yellow",
-            value: "yellow"
-        },
-        {
-            label: "Blue",
-            value: "blue"
-        }
-    ]
-
+    //Used for pub-sub communication
     @wire(CurrentPageReference)
     pageRef;
 
+    //Event handlers
     handleColorSelected(event) {
         this.colorSelectedToCanvas = event.target.value;
     }
