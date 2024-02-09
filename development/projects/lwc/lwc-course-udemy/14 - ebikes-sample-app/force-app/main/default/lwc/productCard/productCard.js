dc0888bbd728fd68f1a655b13505875367e86793
Flávio da Silva Ferreira <fsf.silva.ferreira@gmail.com>
2024-02-09 08:10:49 -0300

    Udemy LWC Course: Section 14 - E-bikes App - Development - 'productCard' refactored.

--- development/projects/lwc/lwc-course-udemy/14 - ebikes-sample-app/force-app/main/default/lwc/productCard/productCard.js
+++ development/projects/lwc/lwc-course-udemy/14 - ebikes-sample-app/force-app/main/default/lwc/productCard/productCard.js
@@ -1,24 +1,21 @@
 import { LightningElement, wire } from 'lwc';
 import { CurrentPageReference } from 'lightning/navigation';
-import { NavigationMixin } from 'lightning/navigation';
-
-/** Wire adapter to load records, utils to extract values. */
+import { NavigationMixin} from 'lightning/navigation';
 import { getRecord } from 'lightning/uiRecordApi';
-
-/** Pub-sub mechanism for sibling component communication. */
 import { registerListener, unregisterAllListeners } from 'c/pubsub';
 
-/** Product__c Schema. */
 import PRODUCT_OBJECT from '@salesforce/schema/Product__c';
 import NAME_FIELD from '@salesforce/schema/Product__c.Name';
 import LEVEL_FIELD from '@salesforce/schema/Product__c.Level__c';
 import CATEGORY_FIELD from '@salesforce/schema/Product__c.Category__c';
 import MATERIAL_FIELD from '@salesforce/schema/Product__c.Material__c';
 import MSRP_FIELD from '@salesforce/schema/Product__c.MSRP__c';
-import PICTURE_URL_FIELD from '@salesforce/schema/Product__c.Picture_URL__c';
+import PICTURE_URL_FIELD from '@salesforce/schema/Product__c.Picture_URL__C';
 
-/** Record fields to load. */
-const fields = [
+
+//Record fields to load
+const fields = 
+[
     NAME_FIELD,
     LEVEL_FIELD,
     CATEGORY_FIELD,
@@ -31,42 +28,52 @@ const fields = [
  * Component to display details of a Product__c.
  */
 export default class ProductCard extends NavigationMixin(LightningElement) {
-    /** Id of Product__c to display. */
+
     recordId;
 
-    @wire(CurrentPageReference) pageRef;
+    @wire(CurrentPageReference)
+    pageRef;
 
-    /** Load the Product__c to display. */
-    @wire(getRecord, { recordId: '$recordId', fields })
+    //Load the Product__c to display
+    @wire(getRecord, {recordId: '$recordId', fields})
     product;
 
+
+    //Lifecycle hooks
     connectedCallback() {
-        registerListener('productSelected', this.handleProductSelected, this);
+        registerListener('productSelected', this.handleProductSelected, this)
     }
 
     disconnectedCallback() {
-        unregisterAllListeners(this);
+        unregisterAllListeners();
     }
 
-    /**
+
+    //Event handlers
+    /*
      * Handler for when a product is selected. When `this.recordId` changes, the @wire
-     * above will detect the change and provision new data.
-     */
+     * above will detect the change and provide new data.
+    */
     handleProductSelected(productId) {
         this.recordId = productId;
-    }
-
+    } 
+    
     handleNavigateToRecord() {
-        this[NavigationMixin.Navigate]({
-            type: 'standard__recordPage',
-            attributes: {
-                recordId: this.recordId,
-                objectApiName: PRODUCT_OBJECT.objectApiName,
-                actionName: 'view'
+        this[NavigationMixin.Navigate](
+            {
+                type: 'standard__recordPage',
+                attributes: //Issue root cause: Mispelling, "attibutes"
+                {
+                    recordId: this.recordId,
+                    objectApiName: PRODUCT_OBJECT.objectApiName,
+                    actionName: 'view'
+                }
             }
-        });
+        )
     }
 
+
+    //Getters and setters
     get noData() {
         return !this.product.error && !this.product.data;
     }
