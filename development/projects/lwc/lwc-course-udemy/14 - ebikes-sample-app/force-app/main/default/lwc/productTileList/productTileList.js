8b17ad6eef2fb857d09d623ae458299f7264e0f4
Flávio da Silva Ferreira <fsf.silva.ferreira@gmail.com>
2024-02-10 12:33:25 -0300

    Udemy LWC Course: Section 14 - E-bikes App - Development - 'productTileList' component refactored.

--- development/projects/lwc/lwc-course-udemy/14 - ebikes-sample-app/force-app/main/default/lwc/productTileList/productTileList.js
+++ development/projects/lwc/lwc-course-udemy/14 - ebikes-sample-app/force-app/main/default/lwc/productTileList/productTileList.js
@@ -1,58 +1,45 @@
-import { LightningElement, api, track, wire } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 import { CurrentPageReference } from 'lightning/navigation';
-
-/** getProducts() method in ProductController Apex class */
+import { registerListener, unregisterAllListeners, fireEvent } from 'c/pubsub';
 import getProducts from '@salesforce/apex/ProductController.getProducts';
 
-/** Pub-sub mechanism for sibling component communication. */
-import { registerListener, unregisterAllListeners, fireEvent } from 'c/pubsub';
 
 /**
  * Container component that loads and displays a list of Product__c records.
  */
 export default class ProductTileList extends LightningElement {
-    /**
-     * Whether to display the search bar.
-     * TODO - normalize value because it may come as a boolean, string or otherwise.
-     */
-    @api searchBarIsVisible = false;
 
-    /**
-     * Whether the product tiles are draggable.
-     * TODO - normalize value because it may come as a boolean, string or otherwise.
-     */
-    @api tilesAreDraggable = false;
-
-    /** Current page in the product list. */
-    @track pageNumber = 1;
-
-    /** The number of items on a page. */
-    @track pageSize;
-
-    /** The total number of items matching the selection. */
-    @track totalItemCount = 0;
-
-    /** JSON.stringified version of filters to pass to apex */
-    @track filters = {};
-
-    @wire(CurrentPageReference) pageRef;
-
-    /**
-     * Load the list of available products.
-     */
-    @wire(getProducts, { filters: '$filters', pageNumber: '$pageNumber' })
+    @wire(CurrentPageReference)
+    pageRef;
+    @wire(getProducts, {filters: '$filters', pageNumber: '$pageNumber'})
     products;
 
+    //Control properties
+    @api
+    searchBarIsVisible = false;
+    @api
+    tilesAreDraggable = false;
+
+    //Paginator properties
+    pageNumber = 1;
+    pageSize;
+    totalItemCount = 0;
+    filters = {};
+
+
+    //Lifecycle hooks
     connectedCallback() {
         registerListener('filterChange', this.handleFilterChange, this);
     }
 
-    handleProductSelected(event) {
-        fireEvent(this.pageRef, 'productSelected', event.detail);
+    disconnectedCallback() {
+        unregisterAllListeners();
     }
 
-    disconnectedCallback() {
-        unregisterAllListeners(this);
+
+    //Event handlers
+    handleProductsSelected(event) {
+        fireEvent(this.pageRef, 'productSelected', event.detail);
     }
 
     handleSearchKeyChange(event) {
@@ -63,7 +50,10 @@ export default class ProductTileList extends LightningElement {
     }
 
     handleFilterChange(filters) {
-        this.filters = { ...filters };
+        console.log('[productTileList] filters parameter in handleFilterChange: ' + filters);
+
+        //Copying filters parameter to filters property (ES6)
+        this.filters = {...filters};
         this.pageNumber = 1;
     }
 
