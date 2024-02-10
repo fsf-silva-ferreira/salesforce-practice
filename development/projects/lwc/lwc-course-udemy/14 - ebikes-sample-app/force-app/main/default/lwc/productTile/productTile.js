f58a50f8cf95d2eff59648f0aba436ed306100cf
Flávio da Silva Ferreira <fsf.silva.ferreira@gmail.com>
2024-02-10 11:23:12 -0300

    Udemy LWC Course: Section 14 - E-bikes App - Development - 'productTile' component refactored.

--- development/projects/lwc/lwc-course-udemy/14 - ebikes-sample-app/force-app/main/default/lwc/productTile/productTile.js
+++ development/projects/lwc/lwc-course-udemy/14 - ebikes-sample-app/force-app/main/default/lwc/productTile/productTile.js
@@ -1,35 +1,55 @@
-import { LightningElement, api, track } from 'lwc';
+import { LightningElement, api } from 'lwc';
+
 
 /**
  * A presentation component to display a Product__c sObject. The provided
- * Product__c data must contain all fields used by this component.
+ * Product__c data must contain all fields used by this component:
+ * - Picture_URL__c
+ * - Name
+ * - MSRP__c
  */
 export default class ProductTile extends LightningElement {
-    /** Whether the tile is draggable. */
-    @api draggable;
 
-    _product;
-    /** Product__c to display. */
+    //Product fields values to display
+    pictureUrl;
+    name;
+    msrp;
+
+    productForGetterAndSetter;
+
+    //Whether the tile is draggable
+    @api
+    draggable;    
+
+
+    //Getters and setters
+
+    //Product__c to display
     @api
     get product() {
-        return this._product;
+        return this.productForGetterAndSetter;
     }
+
     set product(value) {
-        this._product = value;
+        this.productForGetterAndSetter = value;
         this.pictureUrl = value.Picture_URL__c;
         this.name = value.Name;
+        //Issue root cause: Uppercase "C" after underscore
+        //It must be lowercase
         this.msrp = value.MSRP__c;
     }
 
-    /** Product__c field values to display. */
-    @track pictureUrl;
-    @track name;
-    @track msrp;
 
+    //Event handlers
     handleClick() {
-        const selectedEvent = new CustomEvent('selected', {
-            detail: this.product.Id
-        });
+        const selectedEvent = new CustomEvent
+        (
+            'selected', 
+            {
+                detail: this.product.Id
+            }
+        )
+
         this.dispatchEvent(selectedEvent);
     }
 
